3e595613a057b9e0212ea9201408f6f2a94968d0
Andrii Pereverziev <y4t6official@gmail.com>
2018-10-02 13:19:37 +0300

    Add highlights component

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import './resources/styles.css';
 import Header from './components/header_footer/Header';
 import Featured from './components/featured';
 import VenueInfo from './components/venueInfo';
+import Highlight from './components/Highlights';
 
 class App extends Component {
   render() {
     return (
-      <div className="App" style={{ height: "1500px", background: "rgb(57, 189, 235)"}}>
+      <div className="App" style={{ background: "#fff"}}>
         <Header />
         <Featured/>
         <VenueInfo/>
+        <Highlight/>
       </div>
     );
   }
